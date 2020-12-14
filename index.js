9b580570779f3488e4550d6e4d202db24bdf495d
Alexander Krylov <krylov.exceedteam@gmail.com>
2020-12-14 12:18:50 +0300

    error correction

--- index.js
+++ index.js
@@ -4,28 +4,31 @@ const buttonSave = document.querySelector('.button-save');
 
 inputForm.value = listHomes.value;
 
-buttonSave.addEventListener('click', function(event) {
+buttonSave.addEventListener('click', function (event) {
     event.preventDefault();
-    if(inputForm.value.trim() !== "") {
-        document.getElementById(listHomes.selectedIndex).textContent = inputForm.value;
+    if (inputForm.value.trim().length > 0) {
+        document.getElementById(listHomes.selectedIndex).textContent = inputForm.value.trim();
+        inputForm.value = inputForm.value.trim();
     }
     else {
         inputForm.classList.add('is-invalid');
         buttonSave.classList.add('is-invalid');
     }
-}); 
+});
 
-listHomes.addEventListener('click', function(){
-    if(inputForm.classList[2] == 'is-invalid') {
+listHomes.addEventListener('change', function () {
+    if (inputForm.classList[2] == 'is-invalid') {
         inputForm.classList.remove('is-invalid');
         buttonSave.classList.remove('is-invalid');
     }
     inputForm.value = listHomes.value;
 });
 
-inputForm.addEventListener('input', function(event) {
-    if(event.target.value.trim() !== "") {
+inputForm.addEventListener('input', function (event) {
+    if (event.target.value.trim() !== "") {
         inputForm.classList.remove('is-invalid');
         buttonSave.classList.remove('is-invalid');
     }
 });
+
+
