1428860a21c937b194a5b26a1c32813f5447f649
Alexander Krylov <krylov.exceedteam@gmail.com>
2020-12-14 11:58:26 +0300

    added (Bootstrap)

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const listHomes = document.querySelector('.list-homes');
 const inputForm = document.querySelector('.input-form');
-const battonSave = document.querySelector('.button-save');
+const buttonSave = document.querySelector('.button-save');
 
 inputForm.value = listHomes.value;
 
-battonSave.addEventListener('click', function(event) {
+buttonSave.addEventListener('click', function(event) {
     event.preventDefault();
     if(inputForm.value.trim() !== "") {
         document.getElementById(listHomes.selectedIndex).textContent = inputForm.value;
     }
     else {
-        inputForm.classList.add('validation');
-        battonSave.classList.add('validation');
+        inputForm.classList.add('is-invalid');
+        buttonSave.classList.add('is-invalid');
     }
 }); 
 
 listHomes.addEventListener('click', function(){
+    if(inputForm.classList[2] == 'is-invalid') {
+        inputForm.classList.remove('is-invalid');
+        buttonSave.classList.remove('is-invalid');
+    }
     inputForm.value = listHomes.value;
 });
 
 inputForm.addEventListener('input', function(event) {
     if(event.target.value.trim() !== "") {
-        inputForm.classList.remove('validation');
-        battonSave.classList.remove('validation');
+        inputForm.classList.remove('is-invalid');
+        buttonSave.classList.remove('is-invalid');
     }
 });
